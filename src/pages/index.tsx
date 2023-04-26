c0594717f1d2310824fc26292ed1460c32323997
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-04-26 15:18:09 +0200

    Boilerplate setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Kino project in next.js" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div>
+          <h1 className="mark text-center">Lets Go!</h1>
+        </div>
+      </main>
+    </>
+  );
+}
