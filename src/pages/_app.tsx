cc736d7818ee638fdeb64bf27cc4c02c2ba67eba
Rkron91 <robin.kron@lerniastudent.se>
2023-05-03 18:45:59 +0200

    changed divs to fragments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
 import "@/styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </div>
+    </>
   );
 }
