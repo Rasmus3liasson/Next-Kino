e544f95c6e9dd511e3def1419d6a6d27dc1026ba
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-04 21:30:31 +0200

    Made list show the three first screenings of array

--- src/components/ScreeningsHome/index.tsx
+++ src/components/ScreeningsHome/index.tsx
@@ -1,21 +1,34 @@
 import Screening from "../Screening/index";
 import { ScreeningType } from "@/util/types";
-import { useState } from "react";
-import { movieDataArray } from "@/util/mockMovieData";
+import { useState, useEffect } from "react";
+
 import style from "./style.module.scss";
 
 //TODO: Change screenings to a state that fetches data
-const screenings = movieDataArray;
+const howManyScreeningsToShowAtStart = 3;
 
 export default function ScreeningsHome({
   screenings,
 }: {
   screenings: ScreeningType[];
 }) {
+  const [list, setList] = useState<ScreeningType[]>([]);
+  
+  //Sets the initial amount of screenings on render
+  //TODO connect to api
+  useEffect(() => {
+    setList([...screenings.slice(0,3)]);
+    console.log(list);
+  }, []);
+
+  // When the component loads the first time, 3 screenings should show
+  // useEffect to set screenings state to contain the three first indexes from data
+  // when button is clicked, add the rest. 
+
   return (
     <section className={style.container}>
       <h1 className={style.title}>Kommande visningar</h1>
-      {screenings.map((screening: ScreeningType) => (
+      {list.map((screening: ScreeningType) => (
         <Screening movieData={screening} />
       ))}
       <button className={style.button}>Se alla visningar</button>
