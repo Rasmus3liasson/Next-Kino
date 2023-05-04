137ce566adee5a22681796d2b06e936a261a4480
Rkron91 <robin.kron@lerniastudent.se>
2023-05-04 19:35:44 +0200

    states added & password meter added

--- src/components/CreateUserForm.tsx
+++ src/components/CreateUserForm.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import strongPasswordCheck from "@/utils/strongPasswordCheck";
 
 const CreateUserForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [eMail, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pwStrength, setPwStrength] = useState(25);
+
+  useEffect(() => {
+    console.log(pwStrength);
+    console.log(strengthMeter[25].style);
+    setPwStrength(strongPasswordCheck(password));
+  }, [password]);
+
+  type StrengthMeterStyles = {
+    25: {style: string, text: string};
+    50: {style: string, text: string};
+    75: {style: string, text: string};
+    100: {style: string, text: string};
+  };
+
+  const strengthMeter: StrengthMeterStyles = {
+    25: {style: "w-[25%] bg-red-500 ", text: "Very bad"},
+    50: {style: "w-[50%] bg-orange-500 ", text: "Weak"},
+    75: {style: "w-[75%] bg-yellow-500 ", text: "Mediumstrong"},
+    100: {style: "w-[100%] bg-green-500 ", text: "Strong password!"},
+  };
+
   return (
     <form
-      id="createUserForm"
       className="max-w-md mx-auto self-center px-5 my-5 rounded-2xl shadow-lg flex flex-col flex-grow justify-center bg-cblue"
+      id="createUserForm"
     >
       <h1 className="p-5 mx-auto text-4xl text-white font-bold">Skapa konto</h1>
 
@@ -15,11 +44,11 @@ const CreateUserForm = () => {
               Förnamn:
             </label>
             <input
-              id="fName"
-              name="firstName"
-              required
-              type="password"
               className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
+              onChange={(ev) => setFirstName(ev.target.value)}
+              id="fName"
+              required
+              type="text"
               placeholder="Förnamn"
             />
           </div>
@@ -28,27 +57,25 @@ const CreateUserForm = () => {
               Efternamn:
             </label>
             <input
-              id="lName"
-              name="lastName"
-              required
-              type="password"
               className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
+              onChange={(ev) => setLastName(ev.target.value)}
+              id="lName"
+              required
+              type="text"
               placeholder="Efternamn"
             />
           </div>
         </section>
-
         <section className="flex space-x-2 mb-2">
           <div>
             <label htmlFor="uName" className="block mb-1 text-white">
               Användarnamn:
             </label>
             <input
+              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
               id="uName"
-              name="userName"
               required
               type="text"
-              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
               placeholder="Användarnamn"
             />
           </div>
@@ -57,25 +84,28 @@ const CreateUserForm = () => {
               E-mail:
             </label>
             <input
+              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
+              onChange={(ev) => setEmail(ev.target.value)}
               id="eMail"
               required
               type="email"
-              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
               placeholder="E-mail"
             />
           </div>
         </section>
-
         <section className="flex space-x-2">
           <div>
             <label htmlFor="passwordInput" className="block mb-1 text-white">
               Skapa lösenord:
             </label>
             <input
+              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
+              onChange={(ev) => {
+                setPassword(ev.target.value);
+              }}
               id="passwordInput"
               required
               type="password"
-              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue"
               placeholder="Lösenord"
             />
           </div>
@@ -84,33 +114,42 @@ const CreateUserForm = () => {
               Bekräfta lösenord:
             </label>
             <input
+              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue blur:border-cligt"
+              onChange={(ev) => setConfirmPassword(ev.target.value)}
               id="confPassword"
               required
               type="password"
-              className="w-full mb-2 focus:outline-none focus:ring-0 border-4 border-transparent focus:border-clightblue blur:border-cligt"
               placeholder="Lösenord"
             />
           </div>
         </section>
-        <div id="error" className="text-white text-lg"></div>
 
-        <section className="flex  space-x-2 my-3">
+        {password.length > 0 && (
+          <div
+            className={` ${strengthMeter[pwStrength as keyof StrengthMeterStyles].style
+              } px-2 text-center rounded-full `}
+          >
+            {strengthMeter[pwStrength as keyof StrengthMeterStyles].text}
+          </div>
+        )}
+        <div id="error" className="text-white text-lg"></div>
+        {/*         <section className="flex  space-x-2 my-3">
           <label htmlFor="strNeter" className="w-1/2 text-white">
-            Lösenordsstyrka{" "}
+            Lösenordsstyrka
           </label>
           <meter
             id="strMeter"
             className="w-1/2"
             low={0}
-            high={5}
+            high={2}
             value=""
             max="4"
           ></meter>
-        </section>
-
+        </section> */}
+        {/* <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"></div> */}
         <button
-          id="submitBtn"
           className="my-4 w-1/2 self-center bg-clightblue font-semibold  border-2 border-transparent hover:border-white"
+          id="submitBtn"
         >
           bekräfta
         </button>
