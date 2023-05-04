13f2a6d69b78620e5dc59e5278b9980f07d1c630
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-04 16:28:01 +0200

    Add container for screenings and fixed formatting of code

--- src/components/Screening/index.tsx
+++ src/components/Screening/index.tsx
@@ -1,15 +1,25 @@
-import style from './style.module.scss';
-import Image from 'next/image';
-import Link from 'next/link';
-import { ScreeningType } from '@/util/types';
+import style from "./style.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import { ScreeningType } from "@/util/types";
 
-export default function Screening({movieData}: { movieData: ScreeningType }) {
-    return (
-    <Link style={{textDecoration: 'none'}} href='/' className={style.card}>
-      <Image className={style.img} height={120} width={90} src={'/dummy.jpg'} alt={`The poster for ${movieData.title}`} />
+export default function Screening({ movieData }: { movieData: ScreeningType }) {
+  return (
+    <Link style={{ textDecoration: "none" }} href="/" className={style.card}>
+      <Image
+        className={style.img}
+        height={120}
+        width={90}
+        src={"/dummy.jpg"}
+        alt={`The poster for ${movieData.title}`}
+      />
       <h3 className={`${style.title} ${style.cardItem}`}>{movieData.title}</h3>
-      <small className={`${style.date} ${style.cardItem}`}>{movieData.date}</small>
-      <small className={`${style.location} ${style.cardItem}`}>{movieData.location}</small>
+      <small className={`${style.date} ${style.cardItem}`}>
+        {movieData.date}
+      </small>
+      <small className={`${style.location} ${style.cardItem}`}>
+        {movieData.location}
+      </small>
     </Link>
   );
 }
