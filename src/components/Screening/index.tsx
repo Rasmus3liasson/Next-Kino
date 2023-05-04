035212e7450fd08bed3cdfb876a6ca4b1dc95c58
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-04 14:33:50 +0200

    Add styling and content for screening component

--- src/components/Screening/index.tsx
+++ src/components/Screening/index.tsx
@@ -1,21 +1,24 @@
-import style from './style.module.css';
+import style from './style.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export type ScreeningType = {
+type ScreeningType = {
   id: number;
   date: string;
   title: string;
   location: string;
   spokenLang: string;
   subLang: string;
+  poster: string;
 };
 
 export default function Screening({movieData}: { movieData: ScreeningType }) {
-    console.log(movieData);
     return (
-    <>
-      <h3>{movieData.title}</h3>
-      <span>{movieData.date}</span>
-      <span>{movieData.location}</span>
-    </>
+    <Link style={{textDecoration: 'none'}} href='/' className={style.card}>
+      <Image className={style.img} height={120} width={90} src={'/dummy.jpg'} alt={`The poster for ${movieData.title}`} />
+      <h3 className={`${style.title} ${style.cardItem}`}>{movieData.title}</h3>
+      <small className={`${style.date} ${style.cardItem}`}>{movieData.date}</small>
+      <small className={`${style.location} ${style.cardItem}`}>{movieData.location}</small>
+    </Link>
   );
 }
