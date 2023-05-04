bc419026c38f5ac5d14c1dfe8e7bbd5a5d41a525
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-04 20:43:13 +0200

    Fix even spacing on card when title wraps on multiple lines

--- src/util/mockMovieData.tsx
+++ src/util/mockMovieData.tsx
@@ -14,7 +14,7 @@ export const movieData: ScreeningType = {
   [
     {
       id: '1235',
-      date: '21:00 2023-02-27',
+      date: '18:00 2023-02-27',
       title: "The Shawshank Redemption",
       location: 'Stora salongen',
       spokenLang: 'ENG',
@@ -23,17 +23,17 @@ export const movieData: ScreeningType = {
     },
     {
       id: '6589',
-      date: '21:00 2023-02-28',
-      title: "The Shawshank Redemption",
+      date: '19:00 2023-02-27',
+      title: "The Usual Suspects",
       location: 'Matsalen',
       spokenLang: 'ENG',
       subLang: 'SWE',
-      poster: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg'
+      poster: 'https://m.media-amazon.com/images/M/MV5BYTViNjMyNmUtNDFkNC00ZDRlLThmMDUtZDU2YWE4NGI2ZjVmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg'
     },
     {
       id: '3754',
-      date: '21:00 2023-02-26',
-      title: "The Shawshank Redemption",
+      date: '21:00 2023-02-27',
+      title: "Avengers: Endgame",
       location: 'Lilla Salongen',
       spokenLang: 'ENG',
       subLang: 'SWE',
@@ -41,8 +41,8 @@ export const movieData: ScreeningType = {
     },
     {
       id: '9876',
-      date: '21:00 2023-02-27',
-      title: "The Shawshank Redemption",
+      date: '19:00 2023-02-28',
+      title: "Min Granne Totoro",
       location: 'Stora salongen',
       spokenLang: 'ENG',
       subLang: 'SWE',
