acc695def5bd66b808fca2f87ac4dca2e1a8495e
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-05-04 10:54:27 +0200

    added fragment rename varable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,12 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <Component {...pageProps} />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,10 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Lule Northern Light Cinema</title>
-        <meta name="description" content="Kino project in next.js" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-    </>
+    <Head>
+      <title>Lule Northern Light Cinema</title>
+      <meta name="description" content="Kino project in next.js" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
   );
 }
