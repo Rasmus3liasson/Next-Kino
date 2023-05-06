da8978a6f2e05b1866d0335834466938d2d43781
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-06 22:00:20 +0200

    Add mock-apiendpoint for screenings

--- src/pages/api/screenings.ts
+++ src/pages/api/screenings.ts
@@ -1,12 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ScreeningType } from "@/util/types";
+import { movieDataArray } from "@/util/mockMovieData";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ScreeningType>
+  res: NextApiResponse<ScreeningType[]>
 ) {
-    const data: ScreeningType[] = [];
-
+    //TODO: Add database util function that
+    // finds 10 upcoming screenings regardless
+    // of movie.
+    const data = movieDataArray;
 
   res.status(200).json(data);
 }
