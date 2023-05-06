c1e270974344f31ecb928d26806c43fdd6d9d9a2
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-06 22:41:05 +0200

    Refactor api-route to separate "logic" into an exportable function

--- src/pages/api/screenings.ts
+++ src/pages/api/screenings.ts
@@ -2,14 +2,18 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { ScreeningType } from "@/util/types";
 import { movieDataArray } from "@/util/mockMovieData";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ScreeningType[]>
 ) {
-    //TODO: Add database util function that
-    // finds 10 upcoming screenings regardless
-    // of movie.
-    const data = movieDataArray;
-
+  const data = await getData();
+  
   res.status(200).json(data);
+}
+
+export async function getData(){
+  //TODO: Add database util function that
+  // finds 10 upcoming screenings regardless
+  // of movie.
+  return movieDataArray;
 }
