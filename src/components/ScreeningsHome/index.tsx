c2a7ca86e04302d08203c9595e8def0ac0233aaf
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-08 15:58:19 +0200

    Refactor code to use getServerSideProps

--- src/components/ScreeningsHome/index.tsx
+++ src/components/ScreeningsHome/index.tsx
@@ -1,10 +1,8 @@
 import Screening from "../Screening/index";
 import { ScreeningType } from "@/util/types";
-import { useState, useEffect } from "react";
-
+import { useState } from "react";
 import style from "./style.module.scss";
 
-//TODO: Change screenings to a state that fetches data
 const howManyScreeningsToShowAtStart = 3;
 
 export default function ScreeningsHome({
@@ -12,23 +10,14 @@ export default function ScreeningsHome({
 }: {
   screenings: ScreeningType[];
 }) {
-  const [list, setList] = useState<ScreeningType[]>([]);
+  const [expanded, setExpanded] = useState(false);
+  const list = expanded
+    ? screenings
+    : screenings.slice(0, howManyScreeningsToShowAtStart);
 
-  //Sets the initial amount of screenings on render
-  useEffect(() => {
-    setList([...screenings.slice(0, howManyScreeningsToShowAtStart)]);
-    console.log(list);
-  }, []);
   // Handler for updating list with more screenings
   function handleClick() {
-    setList(() => {
-      if (list.length === screenings.length) {
-        return [...list];
-      } else {
-        return [...list, ...screenings.slice(3)];
-      }
-    });
-    console.log(list);
+    expanded ? setExpanded(false) : setExpanded(true);
   }
 
   // [x] When the component loads the first time, 3 screenings should show
@@ -39,10 +28,10 @@ export default function ScreeningsHome({
     <section className={style.container}>
       <h1 className={style.title}>Kommande visningar</h1>
       {list.map((screening: ScreeningType) => (
-        <Screening movieData={screening} />
+        <Screening key={screening.id} movieData={screening} />
       ))}
       <button onClick={handleClick} className={style.button}>
-        Se alla visningar
+        {expanded ? "Dölj visningar" : "Se fler visningar"}
       </button>
     </section>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import Head from "next/head";
-import ScreeningsHome from '../components/ScreeningsHome'
-import { movieData } from "@/util/mockMovieData";
-import { movieDataArray } from "@/util/mockMovieData";
+import ScreeningsHome from "../components/ScreeningsHome";
+import { ScreeningType } from "@/util/types";
+import { GetServerSideProps } from "next";
+import { getData } from "./api/screenings";
+import { FC } from "react";
 
-export default function Home() {
+// This gets called on every request
+export async function getServerSideProps() {
+  const data = await getData();
+  return {
+    props: {
+      screenings: data,
+    },
+  };
+}
+
+export default function Home({ screenings }: { screenings: ScreeningType[] }) {
   return (
     <>
       <Head>
@@ -11,7 +23,7 @@ export default function Home() {
         <meta name="description" content="Kino project in next.js" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <ScreeningsHome screenings={movieDataArray} />
+      <ScreeningsHome screenings={screenings} />
     </>
   );
 }
