8150c0cbc4c990088798ef0b6a1d65a068aac63b
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-08 17:16:02 +0200

    Add jsx and state for updating list of movies.

--- src/components/AllMovies/index.tsx
+++ src/components/AllMovies/index.tsx
@@ -1,23 +1,38 @@
-import style from "style.module.scss";
+import style from "./style.module.scss";
+import { useState } from "react";
+import Movie from "../Movie";
 
 /*
-    Create a container for the component
-        Container component with an subcomponent for each movie 
-    Container should have a Header, a list of links, and a button att the bottom. 
-        use a button at the bottom and style it to look like text. 
+    Create a container for the component X
+        Container component with an subcomponent for each movie X
+    Container should have a Header, a list of links, and a button att the bottom. X
+        use a button at the bottom and style it to look like text.  
     Each item in the list should be a movieposter and be clickable
-        use Link to make each component of movie.
-    the bottom button should expand the list with all the said movies.
-        use a boolean state to expand the list with the props given to the server.
+        use Link to make each component of movie. X
+    the bottom button should expand the list with all the said movies. X
+        use a boolean state to expand the list with the props given to the server. X
 
 */
 
+const howManyMoviesToShowAtStart = 6;
 export default function AllMovies(movies) {
+  const [expanded, setExpanded] = useState(false);
+  const list = expanded ? movies : movies.slice(0, howManyMoviesToShowAtStart);
+
+  function handleClick(){
+    expanded ? setExpanded(false) : setExpanded(true);
+  }
   return (
     <>
-      <h3>På bio just nu</h3>
-
-      <button>Se alla filmer</button>
+      <section className={style.container}>
+        <h1 className={style.title}>På Bio just nu</h1>
+        {list.map((movies) => (
+          <Movie key={movies.id} movieData={movies} />
+        ))}
+        <button onClick={handleClick} className={style.button}>
+          {expanded ? "Dölj filmer" : "Se fler filmer"}
+        </button>
+      </section>
     </>
   );
 }
--- src/components/Movie/index.tsx
+++ src/components/Movie/index.tsx
@@ -1,10 +1,24 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+import style from './style.module.scss';
 
-
-export default function Movie() {
-    return(
-        <>
-        <Link href={'#'} />
-        </>
-    )
+export default function Movie(movieData) {
+    const link = `/movies/${movieData.id}`
+  return (
+    <>
+      <Link
+        style={{ textDecoration: "none" }}
+        href={link}
+        className={style.card}
+      >
+        <Image
+          className={style.img}
+          height={120}
+          width={90}
+          src={"/dummy.jpg"}
+          alt={`The poster for ${movieData.title}`}
+        />
+      </Link>
+    </>
+  );
 }
