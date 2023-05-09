4a6098e073faa85df8965bb210770f93c60f5040
liber09 <linda.bergsangel@hotmail.com>
2023-05-09 05:22:08 +0200

    Adjustmentds for medium screen sizes

--- src/components/Saloon/index.tsx
+++ src/components/Saloon/index.tsx
@@ -10,6 +10,10 @@ export default function Saloon() {
             <div className={style.legend}>
                 <div className={style.seat_legend_free}><Seat></Seat></div>
                 <p className={style.legendText_free}>Tillgänglig</p>
+                <div className={style.seat_legend_unavailable}><Seat></Seat></div>
+                <p className={style.legendText_unavailable}>Ej tillgänglig</p>
+                <div className={style.seat_legend_selected}><Seat></Seat></div>
+                <p className={style.legendText_selected}>Vald</p>
                
             </div>
         </section>
