1c385a5587b7fa2499faf16ecb2fd0dce1e78363
Rkron91 <robin.kron@lerniastudent.se>
2023-05-10 10:56:30 +0200

    api-route first test

--- src/pages/api/CreateNewUser.ts
+++ src/pages/api/CreateNewUser.ts
@@ -1,15 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+//import mongoose from "mongoose";
 
 interface SubmitedUserResponse {
-    userCreated: boolean;
-    errors: string[];
-  }
-  
+  userCreated: boolean;
+  errors: string[];
+}
+
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<SubmitedUserResponse>
 ) {
-  console.log("reached")
-  res.status(200).json({ userCreated: true, errors: ["bonk", "stonk"] });
+
+  const { userName, firstName, lastName, eMail, password } = req.body
+
+  res.status(200).json({ userCreated: true, errors: [userName, password] });
 }
