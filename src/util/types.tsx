423881b22bb8d905547dd8ceca627d8699f0b043
Rkron91 <robin.kron@lerniastudent.se>
2023-05-10 16:36:16 +0200

    moved types out of my components

--- src/util/types.tsx
+++ src/util/types.tsx
@@ -8,3 +8,23 @@ export type ScreeningType = {
     poster: string;
     screeningId: string;
   };
+
+  export type SubmitedUserResponse = {
+    userCreated: boolean;
+    errors: string[];
+  }
+
+  export type User = {
+    userName: string;
+    firstName: string;
+    lastName: string;
+    eMail: string;
+    password: string;
+  };
+  
+  export type StrengthMeterStyles = {
+    25: { style: string; text: string };
+    50: { style: string; text: string };
+    75: { style: string; text: string };
+    100: { style: string; text: string };
+  };
