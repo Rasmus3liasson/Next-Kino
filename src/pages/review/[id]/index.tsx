74de211cf3073757b01d036ff95d575bd0aeb6b1
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-05-11 13:49:14 +0200

    SETUP

---
+++ src/pages/review/[id]/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Review from "@/components/Rating";
+
+import { ScreeningType } from "@/util/types";
+import { getData } from "@/pages/api/screenings";
+
+export async function getServerSideProps() {
+  const data = await getData();
+  return {
+    props: {
+      screenings: data,
+    },
+  };
+}
+
+export default function MovieDetailsPage({
+  screenings,
+}: {
+  screenings: ScreeningType[];
+}) {
+  return (
+    <>
+      <Head>
+        <title>Lule Northern Light Cinema</title>
+        <meta
+          name="This page get you see what screenings this movie has and information about it"
+          content="Kino project in next.js"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Review screenings={screenings} />
+    </>
+  );
+}
