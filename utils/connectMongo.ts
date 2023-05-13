0b31047891329c6a3d854a052738c160285fe0e9
Dan Saa <dansaa8@protonmail.com>
2023-05-13 17:25:55 +0200

    Declare MONGO_URI in the file that uses it

--- utils/connectMongo.ts
+++ utils/connectMongo.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const MONGO_URI =
+  "mongodb://northUser:bGJucFTFliN8qX2Z@ac-gkbjsml-shard-00-00.oxafelt.mongodb.net:27017,ac-gkbjsml-shard-00-01.oxafelt.mongodb.net:27017,ac-gkbjsml-shard-00-02.oxafelt.mongodb.net:27017/?ssl=true&replicaSet=atlas-yrtq9t-shard-0&authSource=admin&retryWrites=true&w=majority";
+
 const connectMongo = async () => {
-  mongoose.connect(process.env.MONGO_URI as string)
-  .catch((error) => {
+  mongoose.connect(MONGO_URI).catch((error) => {
     console.log(error);
   });
 };
