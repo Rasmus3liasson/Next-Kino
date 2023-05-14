41d17e813ffa2ccbb73d6422c733b4845abc88bf
Dan Saa <dansaa8@protonmail.com>
2023-05-14 21:10:45 +0200

    Added mongo URI to .env file

--- src/util/connectMongo.ts
+++ src/util/connectMongo.ts
@@ -1,10 +1,7 @@
-import mongoose from "mongoose";
-
-const MONGO_URI =
-  "mongodb://northUser:bGJucFTFliN8qX2Z@ac-gkbjsml-shard-00-00.oxafelt.mongodb.net:27017,ac-gkbjsml-shard-00-01.oxafelt.mongodb.net:27017,ac-gkbjsml-shard-00-02.oxafelt.mongodb.net:27017/?ssl=true&replicaSet=atlas-yrtq9t-shard-0&authSource=admin&retryWrites=true&w=majority";
+import mongoose from 'mongoose';
 
 const connectMongo = async () => {
-  mongoose.connect(MONGO_URI).catch((error) => {
+  mongoose.connect(process.env.MONGO_URI as string).catch((error) => {
     console.log(error);
   });
 };
