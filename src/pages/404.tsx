814d66be5779cb0c1b44470d437e1fdc30fca941
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-05-14 15:42:38 +0200

    check

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ export default function Custom404() {
   return (
     <section className="section">
       <h1 className="error">404</h1>
-      <div className="page">Ooops!!! Vi kunde inte hitta denna sida!</div>
+      <div className="page">Ooops!!! Vi kunde inte hitta denna sida</div>
       <Link className="back-home" href="/">
         Back to home
       </Link>
