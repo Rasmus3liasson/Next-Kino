89bdbba2e7962e2c2df382615aa5d5f543d79ccf
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-05-14 15:01:43 +0200

    some static pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <section className="section">
+      <h1 className="error">404</h1>
+      <div className="page">Ooops!!! Vi kunde inte hitta denna sida!</div>
+      <Link className="back-home" href="/">
+        Back to home
+      </Link>
+    </section>
+  );
+}
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,91 @@
+import Head from "next/head";
+import style from "./style.module.scss";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Lule Northern Light Cinema</title>
+        <meta name="description" content="Information about our cinema" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <div className={style.aboutUs}>
+        <h1>Om vår biograf</h1>
+        <h2>Vår historia</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          numquam non vitae eos quo dolorem asperiores deserunt aliquam soluta
+          ipsum nobis ducimus harum porro, laudantium repellendus perferendis,
+          quisquam, necessitatibus rerum! Alias laboriosam voluptatem ullam
+          rerum, voluptatibus ad, perspiciatis odio itaque dolores ipsa
+          laudantium numquam nobis sequi similique repellendus nulla fuga neque
+          fugiat, esse consequuntur doloribus autem qui nesciunt at. Iure. Iure
+          neque tempore, minus voluptatibus quaerat sint, enim odit eos ab
+          accusantium accusamus possimus blanditiis. Consequatur facilis
+          voluptas commodi dignissimos amet. Perferendis veritatis, ipsam
+          pariatur deleniti modi voluptas odio rem. Dolores nihil ipsam expedita
+          quam! Accusantium porro iste accusamus nobis incidunt earum voluptatem
+          nam eum possimus deleniti corporis est facilis asperiores cumque non
+          vero in, ab quaerat praesentium, numquam magnam. Est, vel qui velit
+          obcaecati debitis alias adipisci atque cupiditate, aspernatur nihil,
+          iure ipsa exercitationem expedita recusandae nobis minima odio ducimus
+          itaque deleniti. Voluptate ut laborum modi quos mollitia ipsa! Eaque
+          ab distinctio molestiae nemo eius, praesentium doloremque quam quis
+          non at animi molestias et? Officia, cupiditate doloremque mollitia
+          aperiam, voluptates rem, soluta autem culpa reiciendis qui dolor
+          fugiat repellendus? Perferendis minima ea perspiciatis reiciendis
+          voluptate consectetur error explicabo, deserunt recusandae praesentium
+          eum doloribus expedita et architecto culpa at. Ad amet iure facilis
+          iste atque saepe eligendi aliquid a ut. Fuga repellat quos eligendi
+          nobis suscipit reiciendis debitis assumenda molestiae ipsa doloremque
+          cumque reprehenderit sapiente ipsam corporis, veritatis consequatur a
+          vitae error laborum expedita quo doloribus! Repudiandae consectetur
+          commodi assumenda! Animi ipsam ut consectetur pariatur quas? Quia
+          soluta nesciunt voluptatum expedita? Dolores dicta eos eaque veniam
+          voluptate, reiciendis enim ipsum labore quod nulla excepturi neque
+          facilis magnam non autem repellat. Cupiditate assumenda impedit omnis
+          nulla labore laboriosam. Libero maxime et provident ipsam? Aliquid
+          delectus doloribus nostrum, sapiente cumque facilis itaque eligendi,
+          omnis ex voluptatum ipsum soluta optio reiciendis at beatae.
+        </p>
+        <h2>Vår biograf</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+          asperiores, dolore distinctio hic necessitatibus et nobis blanditiis
+          eligendi eum esse unde tenetur? Commodi esse reiciendis magnam et quos
+          doloribus ad. Ut necessitatibus similique sed suscipit dolores
+          asperiores at molestias numquam nemo enim mollitia, nisi excepturi
+          repudiandae incidunt optio, aliquam odio, ab sapiente! Nisi incidunt
+          fugiat sapiente, eius blanditiis distinctio molestias? Saepe possimus,
+          sequi ea eos ad dicta? Porro aliquam, eveniet id at quaerat obcaecati
+          rem inventore minus nobis, dolores labore blanditiis! Impedit,
+          voluptates rem? Voluptas adipisci et voluptatum commodi neque. Facilis
+          maiores velit ea adipisci qui quas inventore dolore placeat tenetur
+          porro sit odio distinctio, explicabo numquam iusto minus commodi ad ab
+          tempora possimus itaque a. Quod accusantium expedita animi. Corrupti
+          fuga asperiores dolor necessitatibus rerum aut est nobis ipsam quia
+          accusantium! Est magnam sapiente quos! Delectus quo eaque nihil vitae,
+          suscipit consectetur, facere eum doloribus ullam quidem tempore
+          eveniet. Natus voluptas assumenda ullam animi dolore quae
+          exercitationem inventore numquam quam illum vero magnam commodi, iure
+          culpa beatae placeat, sit necessitatibus eaque cum at tempora. Nostrum
+          eum atque quo! Labore. Aliquam nulla quasi, ipsum architecto placeat
+          quaerat neque atque modi aspernatur illo deleniti dolor? Pariatur
+          molestias excepturi odit aut consectetur, ea consequuntur incidunt hic
+          modi amet esse autem inventore doloremque. Laboriosam, nostrum dolores
+          inventore placeat velit praesentium corporis, doloribus quibusdam
+          obcaecati explicabo eos? Non quae facilis quasi nihil aliquid harum
+          veritatis beatae? Id repellendus modi doloremque dolores maiores, enim
+          magnam? Praesentium eveniet et velit repudiandae ut, tenetur rem animi
+          quisquam dolorum nemo, aut obcaecati dolores placeat odio! Quibusdam
+          inventore nobis magni adipisci consequatur deserunt ex reprehenderit
+          esse qui animi. Suscipit. Qui nobis consectetur vel sequi quas,
+          assumenda minima, doloremque soluta amet est quidem! Possimus quaerat
+          enim similique et accusantium adipisci, reiciendis sapiente fugit
+          eaque voluptates voluptatem ea illum vitae rerum!
+        </p>
+      </div>
+    </>
+  );
+}
