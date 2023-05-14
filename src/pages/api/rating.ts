51253ecb3f8b94fafb4b194492c131102b97c06a
burrozd <95920902+burrozd@users.noreply.github.com>
2023-05-14 23:47:06 +0200

    rating handler

--- src/pages/api/rating.ts
+++ src/pages/api/rating.ts
@@ -1,10 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next";
+/* import { NextApiRequest, NextApiResponse } from "next";
 import { movieDataArray } from "../../util/mockMovieData";
 const ratingHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  // Retrieve the movie ID from the query parameters
   const { movieId } = req.query;
 
-  // Find the movie in the movieDataArray based on the ID
+  const movie = movieDataArray.find((movie) => movie.id === movieId);
+
+  if (movie) {
+    const { rating } = movie;
+    res.status(200).json({ rating });
+  } else {
+    res.status(404).json({ error: "Movie not found" });
+  }
+};
+
+export default ratingHandler;
+ */
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { movieDataArray } from "../../util/mockMovieData";
+import { useRouter } from "next/router";
+
+const ratingHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const router = useRouter();
+  const { movieId } = router.query;
+
   const movie = movieDataArray.find((movie) => movie.id === movieId);
 
   if (movie) {
