6983a2c21e3833d2c39ba3f35eaa69ccaae42eaa
liber09 <linda.bergsangel@hotmail.com>
2023-05-15 17:48:03 +0200

    Work with payment

--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -4,21 +4,21 @@ export default function Payment() {
     return (
         <section className={style.paymentContainer}>
             <h2 className={style.Headline}>Hur vill du betala?</h2>
-            <img src="" alt="A picture of the movie cover" />
+            <img className={style.movieCover} src="/dummy.jpg" alt="A picture of the movie cover" />
             <ul>
-                <li><input type="radio"/>Betala med mina poäng</li>
-                <li><input type="radio"/>Bankkort</li>
-                <li><input type="radio"/>Swish</li>
+                <li className={style.paymentMethod}><input type="radio"/>Betala med mina poäng</li>
+                <li className={style.paymentMethod}><input type="radio"/>Bankkort</li>
+                <li className={style.paymentMethod}><input type="radio"/>Swish</li>
             </ul>
-            <p>Sparat bankkort</p>
-            <div className="SavedCreditCard">1
-               <img src="../public/visa-logo.png" alt="Image of credit card company"></img>
-           <p className = "cardNumber">**** **** **** 5454</p>
-               <p>Visa</p>
+            <p className ={style.savedCreditCardTitle}>Sparat bankkort</p>
+            <div className="SavedCreditCard">
+                <img src="/visa-logo.png" alt="Image of credit card company"></img>
+                <p className={style.cardNumber}>**** **** **** 5454</p>
+                <p className={style.creditCardIssuer}>Visa</p>
             </div>
-            <p className = "totalAmountToPay">Totalt att betala</p>
-            <button formAction="Submit">Bekräfta</button>
-            <button formAction="Cancel">Avbryt</button>    
+            <p className={style.totalAmountToPay}>Totalt att betala</p>
+            <button className={style.confirmButton} formAction="Submit">Bekräfta</button>
+            <button className={style.cancelButton} formAction="Cancel">Avbryt</button>    
         </section>
     )
 }
