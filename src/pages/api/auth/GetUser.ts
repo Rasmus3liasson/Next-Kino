2c9669e7fcbfbb951554accb11bb3034ad3564dc
Rkron91 <robin.kron@lerniastudent.se>
2023-05-16 20:17:34 +0200

    ...

--- src/pages/api/auth/GetUser.ts
+++ src/pages/api/auth/GetUser.ts
@@ -1,22 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { NextRequest } from "next/server";
 import { verify } from "jsonwebtoken";
 
-
 export default function getUser(
-  req: NextRequest,
+  req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if(req.method === "GET") {
-    const token = req.cookies?.get("AuthToken")
-    if(token) {
-      const decoded = verify(token?.value, process.env.JWTKEY)
-      res.status(200).json(decoded)
-      return
-    }
+  if(req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  if(req) {
+    const token = req.cookies.get("AuthToken")
+
+    const decoded = verify(token.get("AuthToken"), process.env.JWTKEY)
+    return res.status(200).json(decoded)
   } 
-
-  res.status(405).end();
-
 
 }
