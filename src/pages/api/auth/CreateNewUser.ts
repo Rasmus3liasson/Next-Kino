780744be789e4aceb5c1b895ca84f88544e024a9
Rkron91 <robin.kron@lerniastudent.se>
2023-05-16 17:50:48 +0200

    moved file

--- src/pages/api/auth/CreateNewUser.ts
+++ src/pages/api/auth/CreateNewUser.ts
@@ -1,39 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 import { SubmitedUserResponse } from "@/util/types";
-/* prefixed imports */
-//import connectMongoDB from "@/src/*path*"
-//import user from "@/models/user";
+import connectMongo from "@/util/connectMongo";
+import User from "../../../../models/user";
+import { UserType } from "@/util/types";
 
-export default async function handler(
+
+export default async function createNewUser(
   req: NextApiRequest,
   res: NextApiResponse<SubmitedUserResponse>
 ) {
-  // connectMongoDB is not available atm, waiting for database import
-  // row 22,24,25,37 & 46 is based on planned Schemas and connectionapproach
+ 
   try {
-    await connectMongoDB();
-    const { userName, firstName, lastName, eMail, password } = req.body
-    const userNameTaken = await User.Find({ userName: userName })
-    const eMailTaken = await User.Find({ email: eMail });
+    await connectMongo();
+    const { userName, name, email, password }: UserType = req.body
+    const userNameTaken = await User.findOne({ userName: userName })
+    const eMailTaken = await User.findOne({ email: email });
     const errorMessages = []
-
 
     if (userNameTaken || eMailTaken) {
 
       userNameTaken && errorMessages.push("Username already exist")
       eMailTaken && errorMessages.push("Email already exist")
       res.status(409).json({ userCreated: false, errors: errorMessages })
-
     } else {
 
       const newUser = new User({
+        name: name,
         userName,
-        firstName,
-        lastName,
-        eMail,
-        password: await bcrypt.hash(password, 2)
-
+        email,
+        passwordHash: await bcrypt.hash(password, 2)
       });
 
       await newUser.save()
@@ -46,10 +42,3 @@ export default async function handler(
 
 
 }
-
-
-
-
-
-
-
