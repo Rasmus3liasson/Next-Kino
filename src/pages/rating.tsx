0ba0cc65d235d51c4c850f5e0b224b96f5cccb91
burrozd <95920902+burrozd@users.noreply.github.com>
2023-05-17 16:54:59 +0200

    filee move

--- src/pages/rating.tsx
+++ src/pages/rating.tsx
@@ -17,8 +17,8 @@ const ratingHandler = (req: NextApiRequest, res: NextApiResponse) => {
 export default ratingHandler;
  */
 
-import { NextApiRequest, NextApiResponse } from "next";
-import { movieDataArray } from "../../util/mockMovieData";
+/* import { NextApiRequest, NextApiResponse } from "next";
+import { movieDataArray } from "../util/mockMovieData";
 
 const ratingHandler = (req: NextApiRequest, res: NextApiResponse) => {
   const { movieId } = req.query;
@@ -33,6 +33,34 @@ const ratingHandler = (req: NextApiRequest, res: NextApiResponse) => {
     const { rating } = movie;
     res.status(200).json({ rating });
   } else {
+    res.status(404).json({ error: "Movie not found" });
+  }
+};
+
+export default ratingHandler;
+ */
+
+import Rating from "../../src/components/Rating/index";
+import { NextApiRequest, NextApiResponse } from "next";
+import { movieDataArray } from "../util/mockMovieData";
+
+const ratingHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { movieId } = req.query;
+
+  if (!movieId) {
+    return res.status(400).json({ error: "Movie ID is required" });
+  }
+
+  const movie = movieDataArray.find((movie) => movie.id === movieId);
+
+  if (movie) {
+    const { rating } = movie;
+    console.log("Movie found:", movie);
+    console.log("Rating:", rating);
+    const ratingComponent = <Rating movieData={movie} rating={rating} />;
+    res.status(200).send(ratingComponent);
+  } else {
+    console.log("Movie not found");
     res.status(404).json({ error: "Movie not found" });
   }
 };
