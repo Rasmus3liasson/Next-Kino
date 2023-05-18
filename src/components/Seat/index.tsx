c065b06de8ea2ac1597d44fe3813d92e697efae2
liber09 <linda.bergsangel@hotmail.com>
2023-05-18 08:10:19 +0200

    counter

--- src/components/Seat/index.tsx
+++ src/components/Seat/index.tsx
@@ -3,10 +3,16 @@ import style from "./style.module.scss";
 import React from "react";
 
 export default function Seat() {
-    const [selected, setSelected] = useState(false);  
+    const [selected, setSelected] = useState(false);
+    const [counter, setSeatCount] = useState(0);  
 
     const handleClick = () => {
         setSelected(!selected)
+        if (selected){
+            counter + 1
+        }else{
+            counter-1
+        }
       }
 
     return (
