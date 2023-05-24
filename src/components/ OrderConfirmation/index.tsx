d164dc53c20032120e5e62cbba4debb80dee0b9a
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-05-24 18:58:44 +0200

    order confirmed finished

--- src/components/ OrderConfirmation/index.tsx
+++ src/components/ OrderConfirmation/index.tsx
@@ -1,32 +1,54 @@
 import style from "./style.module.scss";
-import React from "react";
+import React, { useContext } from "react";
 import Router from "next/router";
-import { User } from "@/util/mockUser";
+import Image from "next/image";
+import { accountStateContext } from "@/pages/_app";
 
 export default function OrderConfirmation() {
-    const link = `/`;
+  const { accountState } = useContext(accountStateContext);
+  const link = `/`;
 
-    function handleClick() {
-        Router.push(link);
-      }
-      
-    return (
-        <section className={style.container}>
-            <img src="/order-confirmed.png" alt="Big green circle with a checkmark inside" />
-            <h2 className={style.Headline}>Order bekräftad</h2>
-            <div className ={style.userInfo}>
-                <p>Namn</p>
-                <p>Stig björn Jansson</p>
-                <p>Mobilnummer</p>
-                <p>0701234456</p>
-                <p>Email</p>
-                <p>stig-bjorn.jansson@stiiiiig.nu</p>
-                <p>Ordernummer</p>
-                <p>127354424</p>
-                <p>Orderdatum</p>
-                <p>2023-02-27</p>
-            </div>
-            <button onClick={handleClick} className={style.returnButton} formAction="">Tillbaka till startsida</button>    
-        </section>
-    )
+  function handleClick() {
+    Router.push(link);
+  }
+
+  //random order number
+  const min = 10000; // Minimum 5-digit number
+  const max = 99999; // Maximum 5-digit number
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  //format date
+  const date = new Date();
+  const year = date.getFullYear();
+  const orderDate = date.getDate() + "/" + date.getMonth() + "-" + year;
+
+  return (
+    <section className={style.container}>
+      <Image
+        src="/order-confirmed.png"
+        alt="Big green circle with a checkmark inside"
+        width={300}
+        height={300}
+      />
+      <h2 className={style.Headline}>Order bekräftad</h2>
+      <div className={style.userInfo}>
+        <p>Namn</p>
+        <p>{accountState?.name.first + " " + accountState?.name.last}</p>
+
+        <p>Email</p>
+        <p>{accountState?.email}</p>
+        <p>Ordernummer</p>
+        <p>{randomNumber}</p>
+        <p>Orderdatum</p>
+        <p>{orderDate}</p>
+      </div>
+      <button
+        onClick={handleClick}
+        className={style.returnButton}
+        formAction=""
+      >
+        Tillbaka till startsida
+      </button>
+    </section>
+  );
 }
