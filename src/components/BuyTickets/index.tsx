6b8ee0b24e419609edb9b5afd28cf7b289f57da9
liber09 <linda.bergsangel@hotmail.com>
2023-05-27 22:40:51 +0200

    image and other moviedata working

--- src/components/BuyTickets/index.tsx
+++ src/components/BuyTickets/index.tsx
@@ -1,12 +1,13 @@
 import style from "./style.module.scss";
 import { ScreeningProps } from "@/types/screeningTypes";
-import React from "react";
+import React, { useState } from "react";
 import  { useRouter } from 'next/router';
 import { NumberContextProvider } from "@/util/NumberContext";
 import { MovieProps } from "@/util/types";
 
 export default function BuyTickets({ screenings, selectedSeatIds, id, displayDate, movieData}: { screenings: ScreeningProps, selectedSeatIds: number[], id: string, displayDate: string, movieData: MovieProps}) {
-  console.log("selectedSeats: " + selectedSeatIds);
+  const [seats, setSeats] = useState(selectedSeatIds)
+  console.log("BuyTickets: "+ seats)
   let dateAndTime = new Date(parseInt(displayDate))
   const ISOdate = dateAndTime.toISOString();
   const offsetFormattedDate = ISOdate.replace("Z", "+00:00");
@@ -14,6 +15,8 @@ export default function BuyTickets({ screenings, selectedSeatIds, id, displayDat
   const Link = `/movies/${id}/booking/${offsetFormattedDate}/payment?${selectedSeatIds}`;
   const router = useRouter();
   function handleClick() {
+    setSeats(selectedSeatIds)
+    console.log("BuyTickets_after: " + seats)
     router.push(Link);
   }
   return (
@@ -21,9 +24,9 @@ export default function BuyTickets({ screenings, selectedSeatIds, id, displayDat
     <div className={style.container}>
       <NumberContextProvider numberArray={selectedSeatIds}></NumberContextProvider>
       <hr className={style.divider}/>
-      <h2 className={style.title }>{screenings.title}</h2>
+      <h2 className={style.title }>{movieData.title}</h2>
       <p className={style.date}> Tid och datum: {dateAndTimeString}</p>
-      <p className={style.date}> Plats: {screenings.location}</p>
+      <p className={style.date}> Plats: {"Stora salongen"}</p>
       <button onClick={handleClick} className={style.confirmButton} formAction="Submit">Till kassan</button>
       <button className={style.cancelButton} formAction="Cancel">Avbryt</button>  
     </div>
