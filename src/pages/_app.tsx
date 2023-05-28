03954e96c6055387e0a1199d671b8d609d0852ce
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-05-28 15:52:31 +0200

    change to GSSP for token in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,9 @@ import { AppProps } from "next/app";
 import React, { createContext, useState } from "react";
 import validateAuthToken from "@/util/validateAuthToken";
 import { userData } from "../types/userTypes";
-import { NextPageContext } from "next";
 import { parse } from "cookie";
 import { loginModalContext } from "@/util/loginModalContext";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
 export const accountStateContext = createContext<{
   accountState: userData | null;
@@ -16,6 +16,32 @@ export const accountStateContext = createContext<{
   accountState: null,
   setAccountState: () => {},
 });
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const cookie = parse(context.req?.headers.cookie || "");
+  const token = await validateAuthToken(cookie.value);
+  if (token) {
+    const { name, email, userName } = token;
+    return {
+      props: {
+        user: {
+          name,
+          email,
+          userName,
+        },
+      },
+    };
+  }
+
+  // needs this so it dont complain even if the user props is empty
+  else {
+    return {
+      props: {},
+    };
+  }
+};
 
 function App({ Component, pageProps }: AppProps) {
   const [accountState, setAccountState] = useState<userData | null>(null);
@@ -35,7 +61,7 @@ function App({ Component, pageProps }: AppProps) {
     </>
   );
 }
-
+/* 
 App.getInitialProps = async ({ ctx }: { ctx: NextPageContext }) => {
   const cookie = parse(ctx.req?.headers.cookie || "");
   const token = await validateAuthToken(cookie.value);
@@ -49,6 +75,6 @@ App.getInitialProps = async ({ ctx }: { ctx: NextPageContext }) => {
       },
     };
   } else return {};
-};
+}; */
 
 export default App;
