10717c206af94e93b26ae434dbdee4e1d83f75c4
Dan Saa <dansaa8@protonmail.com>
2023-05-29 14:52:11 +0200

    Remove maxPoolSize

--- src/util/connectMongo.ts
+++ src/util/connectMongo.ts
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
 const connectMongo = async () => {
-  mongoose.connect(process.env.MONGO_URI as string, {
-    maxPoolSize: 1000
-  }).catch((error) => {
+  mongoose.connect(process.env.MONGO_URI as string, {}).catch((error) => {
     console.log(error);
   });
 };
